e9530e7668187599d67716f5b800877b554c1db1
TekerzRobotics <ftc21764@gmail.com>
2024-11-12 19:46:49 -0800

    0.13
    made it so the slide moves out a little and then puts the
    wrist down
    
    -Zach, Max

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeWrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeWrist.java
@@ -1,8 +1,8 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -18,20 +18,20 @@ public class IntakeWrist {
 
     static final int LOW_HARDSTOP = 0;
     static final int HIGH_HARDSTOP = 125; // placeholder
-    static final double MAX_SPEED = 0.5;
+    static final double MAX_SPEED = 0.2;
     // add statics as necessary
 
     static final int POSITION_TO_INTAKE = -117;
-    static final int POSITION_TO_DELIVER = -84;
+    static final int POSITION_TO_REST = -10;
     final boolean isAutonomous; // will not be necessary if wrist is automated
 
     public IntakeWrist(HardwareMap hardwareMap, Telemetry telemetry, Gamepad gamepad, boolean isAutonomous) {
 
 
 
-        this.gamepad = gamepad;
+        this.gamepad      = gamepad;
         this.isAutonomous = isAutonomous;
-        this.telemetry= telemetry;
+        this.telemetry    = telemetry;
 
         wristMotor = hardwareMap.get(DcMotor.class, "wristMotor"); // port 1
         wristMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -52,13 +52,23 @@ public class IntakeWrist {
     }
 
     private void readGamepad(Gamepad gamepad) {
-       if(gamepad.dpad_up && IntakeSlide.intakeSlideMotor.getCurrentPosition() > 300){
-        setPosition(POSITION_TO_DELIVER);
+       if(gamepad.dpad_up && IntakeSlide.intakeSlideMotor.getCurrentPosition() > 300) {
+           wristMotor.setPower(MAX_SPEED);
+           while (!(wristMotor.getCurrentPosition() >= POSITION_TO_REST)) {setPosition(POSITION_TO_REST);}
+           wristMotor.setPower(0);
        }
-       if(gamepad.dpad_down && IntakeSlide.intakeSlideMotor.getCurrentPosition() > 300){
-               setPosition(POSITION_TO_INTAKE);
-           }
+       if(gamepad.dpad_down && IntakeSlide.intakeSlideMotor.getCurrentPosition() > 300) {
+           wristMotor.setPower(MAX_SPEED);
+           while (!(wristMotor.getCurrentPosition() <= POSITION_TO_INTAKE)) {setPosition(POSITION_TO_INTAKE);}
+           wristMotor.setPower(0);
        }
+       else if (gamepad.dpad_down && IntakeSlide.intakeSlideMotor.getCurrentPosition() < 300){
+           IntakeSlide.intakeSlideMotor.setTargetPosition(350);
+           wristMotor.setPower(MAX_SPEED);
+           while (!(wristMotor.getCurrentPosition() <= POSITION_TO_INTAKE)) {setPosition(POSITION_TO_INTAKE);}
+           wristMotor.setPower(0);
+       }
+    }
 
 
     public void loop() {
